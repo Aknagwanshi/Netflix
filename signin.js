b1db494a625bd26c9998538c94d4a12ab4389c13
Aniket Kumar Nagvanshi <121690677+Aknagwanshi@users.noreply.github.com>
2024-05-11 16:02:34 +0530

    Update signin.js

--- signin.js
+++ signin.js
@@ -1,13 +1,7 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-app.js";
 import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.11.1/firebase-auth.js";
 const firebaseConfig = {
-    apiKey: "AIzaSyC5G3bPfGk9zTDD-DAUoGhvcMMy7llH1KQ",
-    authDomain: "netflix-50336.firebaseapp.com",
-    projectId: "netflix-50336",
-    storageBucket: "netflix-50336.appspot.com",
-    messagingSenderId: "465725742307",
-    appId: "1:465725742307:web:c30886bf0db651e194b9d6",
-    measurementId: "G-69932FVYHP"
+<Your>
 };
 
 // Initialize Firebase
